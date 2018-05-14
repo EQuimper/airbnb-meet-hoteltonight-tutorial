df083cf2d5f268ecb29a4f1f96f52e8580906ad1
Emanuel Quimper <quimperemanuel@gmail.com>
2018-05-14 09:59:28 -0400

    Remove date from user

--- server/src/modules/user/resolvers.ts
+++ server/src/modules/user/resolvers.ts
@@ -1,11 +1,9 @@
 import { ResolverMap } from '../../types/graphql-utils';
 import { getViewer } from '.';
 import { getOwnerPlaces } from '..';
-import { GraphQLDateTime } from 'graphql-iso-date';
 
 // @ts-ignore
 const resolvers: ResolverMap = {
-  Date: GraphQLDateTime,
   User: {
     places: ({ _id }) => getOwnerPlaces(_id),
   },
--- server/src/types/index.d.ts
+++ server/src/types/index.d.ts
@@ -1,2 +1,1 @@
 declare module 'merge-graphql-schemas';
-declare module 'mongodb-memory-server';
